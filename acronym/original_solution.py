0f0b88aabea07d20d1aac3fa1ed7329be3ddd043
Danylo-Ik <daniel.ik0806@gmail.com>
2024-04-18 22:31:42 +0300

    acronym

---
+++ acronym/original_solution.py
@@ -0,0 +1,27 @@
+'''
+This function takes the first letter of each word in a string and returns an acronym
+'''
+import timeit
+
+@profile
+def create_acronym(message: str) -> str:
+    if not isinstance(message, str):
+        return None
+
+    acronym = ""
+    sentences = message.split('\\n')
+
+    for sentence in sentences:
+        words = sentence.split()
+        for word in words:
+            if not word.isalpha():
+                return None
+            acronym += word[0].upper()
+        acronym += ' - ' + sentence
+        if sentence != sentences[-1]:
+            acronym += '\\n'
+
+    return acronym
+
+if __name__ == '__main__':
+    create_acronym("Факультет Прикладних Наук Українського Католицького Університету")
